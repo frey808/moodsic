36947517627695ea85fdb1269fca6ea4f20f048f
ablaspinto <ablaspinto@gmail.com>
2023-09-16 18:27:48 -0400

    ai stuff

---
+++ index.js
@@ -0,0 +1,58 @@
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+export default function Home() {
+  const [songInput, setSongInput] = useState("");
+  const [result, setResult] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ song: songInput }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+
+      setResult(data.result);
+      setSongInput("");
+    } catch(error) {
+      // Consider implementing your own error handling logic here
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>OpenAI Quickstart</title>
+        <link rel="icon" href="/mood.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <img src="/mood.png" className={styles.icon} />
+        <h3>Moodsic</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="song"
+            placeholder="What mood are you in?"
+            value={songInput}
+            onChange={(e) => setSongInput(e.target.value)}
+          />
+          <input type="submit" value="Generate Songs" />
+        </form>
+        <div className={styles.result}>{result}</div>
+      </main>
+    </div>
+  );
+}
